8744470529f55cc0554718749bc367b4f576c22d
Tyler Richards <tyler@tyler-richards.com>
2021-05-21 11:28:55 -0400

    dev env fix

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
+require('dotenv').config();
+
 
 const session = require('express-session');
 const express = require('express');
