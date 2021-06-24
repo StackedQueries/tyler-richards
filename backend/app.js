c801f3673c94dd81012613075c0ac807625e51ed
Tyler Richards <tyler@tyler-richards.com>
2021-06-24 17:05:30 -0400

    test

--- backend/app.js
+++ backend/app.js
@@ -33,6 +33,7 @@ var allowedOrigins = ['http://localhost:3000',
         origin: function (origin, callback) {    // allow requests with no origin 
             // (like mobile apps or curl requests)
             if (!origin) return callback(null, true); if (allowedOrigins.indexOf(origin) === -1) {
+                console.log(origin);
                 var msg = 'The CORS policy for this site does not ' +
                     'allow access from the specified Origin.';
                 return callback(new Error(msg), false);
