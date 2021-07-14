c2ff90abcb5aea7756be119de6dd9d2fb91ad75a
Tyler Richards <tyler@tyler-richards.com>
2021-07-14 16:50:48 -0400

    update landing and add DWD

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -2,9 +2,11 @@ import '../styles/landing.scss'
 import Nav from './Nav'
 import React, { useState, useEffect, useRef } from "react";
 import { getQuote } from '../api';
+import { GetCurrentSize } from '../utils/ViewportProvider';
 const Landing = () => {
-
+    const size = GetCurrentSize();
     const locale = 'en';
+    const [secondQuote, setSecondQuote] = useState(true);
     const [today, setDate] = useState(new Date());
     const [quote, setQuote] = useState({
         q: '',
@@ -26,6 +28,11 @@ const Landing = () => {
         }
     }, []);
 
+    useEffect(()=>{
+        if (size !== "large"){
+            setSecondQuote(false)
+        }
+    }, [size])
 
     const day = today.toLocaleDateString(locale, { weekday: 'long' });
     const date = `${day}, ${today.getDate()} ${today.toLocaleDateString(locale, { month: 'long' })}\n\n`;
@@ -44,7 +51,9 @@ const Landing = () => {
                 <Nav classItems="headerNav" />
                 <div className='linebox'>
                     <p className='side'>{date}</p>
-                    <p className='funbox '><span className="sliding">{quote.q} - {quote.a}</span><span className="sliding">{quote.q} - {quote.a}</span></p>
+                    <p className='funbox '>
+                        <span className="sliding">{quote.q} - {quote.a}</span>
+                        {secondQuote && <span className="sliding">{quote.q} - {quote.a}</span>}</p>
 
                     <p className='side'>{time}</p>
                 </div>
